6f6d0e499ab46d2af13cc71f85d0ca1c5d714ee9
Archilipeper <p.archila@uniandes.edu.co>
2025-04-11 07:47:36 -0500

    Funciones de rotacion y cambio de color (Falta la de cambiar el color de la raiz y sus hijos)

--- DataStructures/Tree/red_black_tree.py
+++ DataStructures/Tree/red_black_tree.py
@@ -1,2 +1,22 @@
 def new_map():
     return {"root": None}
+
+def rotate_left(node_rbt):
+    new_right =  node_rbt["right"]["left"]
+    new_root = node_rbt["right"]
+    node_rbt["right"] = new_right
+    new_root["left"] = node_rbt
+    return new_root
+
+def rotate_right(node_rbt):
+    new_root = node_rbt["left"]
+    new_left = new_root["right"]
+    node_rbt["left"] = new_left
+    new_root["right"] = node_rbt
+
+def flip_node_color(node_rbt):
+    if node_rbt["color"] == "RED":
+        node_rbt["color"] = "BLACK"
+    elif node_rbt["color"] == "BLACK":
+        node_rbt["color"] = "RED"
+    return node_rbt
